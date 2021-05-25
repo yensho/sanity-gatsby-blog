bb8cd9b35160d14eae39157a3ee0a5ca8b32def0
Stewart Font <yensho@gmail.com>
2021-05-25 13:35:40 -0500

    fix serializer

--- web/src/components/serializers.js
+++ web/src/components/serializers.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Figure } from "./Figure";
+import PortableText from "./portableText";
 
 const serializers = {
   types: {
     /* eslint-disable-next-line react/display-name */
     authorReference: ({ node }) => <span>{node.author.name}</span>,
     mainImage: Figure,
-    excerptReference: ({ node }) => <span>{node.excerpt.body}</span>,
+    excerptReference: ({ node }) => <span><PortableText blocks={node.excerpt.body} /></span>,
   },
 };
 
